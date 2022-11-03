93cc9edce4c464c1be14c88393052aa29b45a5a0
jayaantara10 <jayaantara10@gmail.com>
2022-11-03 12:08:55 +0800

    Initial commit

---
+++ app/src/main/java/id/jayaantara/rupabali/MainActivity.kt
@@ -0,0 +1,43 @@
+package id.jayaantara.rupabali
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import id.jayaantara.rupabali.ui.theme.RupaBaliTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            RupaBaliTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    RupaBaliTheme {
+        Greeting("Android")
+    }
+}
